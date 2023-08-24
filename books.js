215b4106bb1fe6ca7f8945cafcd7431ecdc451b4
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-24 16:51:13 -0600

    added more books to book file

---
+++ books.js
@@ -0,0 +1,48 @@
+const books=[{
+    "id": "1" ,
+    "title": "One Hundred Years of Solitude" ,
+    "author": "Gabriel Garcia Marquez",
+    "publish_date": "May 1, 1967",
+    "genre":"Magic Realism" ,
+},{
+    "id": "2" ,
+    "title":  "Midnight's Children's",
+    "author": "Salman Rushdie",
+    "publish_date":"1981" ,
+    "genre": "Magic Realism" ,
+},
+{
+    "id": "3" ,
+    "title": "Beloved" ,
+    "author": "Toni Morrison",
+    "publish_date": "1987",
+        "genre": "Magic Realism",
+    },
+{
+    "id": "3" ,
+    "title": "Like Water for Chocolate" ,
+    "author": "Laura Esquivel" ,
+    "publish_date": "1989" ,
+    "genre": "Magic Realism",
+},
+{
+    "id": "3" ,
+    "title": "The Wind-Up Chronicle",
+    "author":"Haruki Murakami",
+    "publish_date": "1994",
+    "genre": "Magic Realism",
+},
+{  
+    "id":  ,
+"title":  ,
+"author": ,
+"publish_date": ,
+"genre": ,},
+{
+    "id":  ,
+    "title":  ,
+    "author": ,
+    "publish_date": ,
+    "genre": ,
+}
+]
