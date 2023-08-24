6ec33909296ff6e54f65497b0eb45fde3c22b715
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-24 16:47:43 -0600

    adding book list to book js

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ import cors from "cors";
 import 'dotenv/config';
 import path from 'path';
 
-
 const app = express();
 
-
 app.use(cors());
-const_dirname =path.resolve();
+
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 2023;
 
-app.get('/', (req, resp) => {
-    resp.send("Hola, Everyone this is my HomePage")
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`this app is listening ${PORT} server`);
-    res.sendFile(path.join(__dirname, 'index.html'))
-})
+
+
+    console.log(`This app is listening to port ${PORT} `);
+});
