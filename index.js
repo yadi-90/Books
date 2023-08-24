c4fda81b4f28c615db341e32985982f312c1e031
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-24 15:32:05 -0600

    adding path to html

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from "express";
 import cors from "cors";
+import 'dotenv/config';
+import path from 'path';
+
+
 const app = express();
-const port = 8080;
+
 
 app.use(cors());
+const PORT = process.env.PORT || 2023
 
 app.get('/', (req, resp) => {
     resp.send("Hola, Everyone this is my HomePage")
 });
 
-app.listen(port, () => {
-    console.log(`this app is listening ${port} port`);
+app.listen(PORT, () => {
+    console.log(`this app is listening ${PORT} server`);
 })
