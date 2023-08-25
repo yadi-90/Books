51fb3f4f1eb7a1c51be9ce4eb24ec3cf34d337a2
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-24 18:29:36 -0600

    add style to homepage

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import path from 'path';
-import BOOKS from "/books";
+import BOOKS from '/books.js';
 
 const app = express();
 
