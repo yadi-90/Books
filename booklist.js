3f1c76ddce77e13f4ea5dde6665b6459fa504cf1
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-24 19:36:21 -0600

    added style to css but there is a bug

--- booklist.js
+++ booklist.js
@@ -1,3 +1,27 @@
-import BOOKS from "../books";
+
+import BOOKS from "./books";
+
+const bookList = document.getElementById("bookList");
+
+
+BOOKS.forEach(book => {
+    const bookContainer = document.createElement("div");
+    bookContainer.classList.add("book");
+
+
+    const bookDetails = document.createElement("div");
+    bookDetails.innerHTML = `
+        <h2>${book.title}</h2>
+        <p><strong>Author:</strong> ${book.author}</p>
+        <p><strong>Published:</strong> ${book.publish_date}</p>
+        <p><strong>Genre:</strong> ${book.genre}</p>
+    `;
+
+    bookContainer.appendChild(bookImage);
+    bookContainer.appendChild(bookDetails);
+
+    bookList.appendChild(bookContainer);
+});
+
 
 
