1241265fb1e5c0ba68ab1078db416f72b9cec639
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-25 10:02:19 -0600

    save changes

--- MagicRealism/src/App.jsx
+++ MagicRealism/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'vite/modulepreload-polyfill'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Magic Realism </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
