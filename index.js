a5cb7a02ff32685a464530d25b42a7c87b5594ce
yadi-90 <126985685+yadi-90@users.noreply.github.com>
2023-08-27 08:45:43 -0600

    added js file to public folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import path from 'path';
-import BOOKS from './books.js'; 
+import BOOKS from "./books.js"; 
 
 const app = express();
 
@@ -12,22 +12,23 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 2023;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public'))); // assuming your HTML and JS files are in the public folder
 
 app.get('/books',function(req, res){
-  res.sendFile('books.html');
+  res.sendFile(path.join(__dirname, 'public', 'books.html')); 
+});
 
 app.get('/api/books', (req, res) => {
     res.json(BOOKS);
 });
 
 app.get('/', (req, res) => {
-  res.sendFile('/index.html' );
+  res.sendFile(path.join(__dirname, 'public', 'index.html')); 
 });
 
 app.get('/api/books/:id', (req, res) => {
   const bookId = req.params.id;
-  const book = BOOKS.find(book => book.id == bookId); 
+  const book = BOOKS.find(book => book.id == bookId); // use == instead of ===
 
   if (book) {
     res.json(book);
@@ -39,3 +40,4 @@ app.get('/api/books/:id', (req, res) => {
 app.listen(PORT, () => {
   console.log(`This app is listening to port ${PORT}`);
 });
+
